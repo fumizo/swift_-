039ae6ad4ce1157117c25d94f8a2e62d078d7369
fumiko <fumi811bbc@gmail.com>
2014-11-15 15:42:52 +0900

    Initial Commit

---
+++ swiftCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftCount
+//
+//  Created by 山本文子 on 2014/11/15.
+//  Copyright (c) 2014年 山本文子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
